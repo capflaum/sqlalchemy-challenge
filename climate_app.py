8bfdaacdc13a1ed2b7f3e273fdf6ea6a298997c0
capflaum <69134400+capflaum@users.noreply.github.com>
2020-10-27 21:01:42 -0700

    latest commit

--- climate_app.py
+++ climate_app.py
@@ -20,7 +20,6 @@ session = Session(engine)
 
 app = Flask(__name__)
 
-
 #Flask Routes
 
 @app.route("/")
@@ -28,27 +27,25 @@ def home():
     """Queries from Climate Activity"""
     return(
     f"All Available Routes:<br/>"
+
     "<br/>"
     f"/api/v1.0/precipitation<br/>"
     f"Precipitation data from previous year<br/>"
-    # Convert the query results to a dictionary using date as the key 
-    # and prcp as the value.
-    #Return the JSON representation of your dictionary.
+
     "<br/>"
     f"/api/v1.0/stations<br/>"
     f"List of stations<br/>"
-    # Return a JSON list of stations from the dataset.
+
     "<br/>"
     f"/api/v1.0/tobs<br/>"
     f"Dates and temperature data from the most active station of previous year<br/>"
-    # Query the dates and temperature observations of the most active station 
-    # for the last year of data.
-    # Return a JSON list of temperature observations (TOBS) for the previous year.
+
     "<br/>"
     f"/api/v1.0/<start><br/>"
     f"Minimum, average, and maximum temperature from start date<br/>"
     
     "<br/>"
+    f"Input requested dates as endpoints (dd-mm-yyy): [start_date]/[end_date]<br/>"
     f"/api/v1.0/<start>/<end><br/>"
     f"Minimum, average, and maximum temperature for given time frame<br/>"
     # Return a JSON list of the minimum temperature, the average temperature, 
@@ -118,42 +115,57 @@ def temperature():
         temp_list.append(temp_dict)
     return jsonify(temp_list)
 
-@app.route("/api/v1.0/<start>", defaults={'end': None})
-@app.route("/api/v1.0/<start>/<end>")
-def startdate_enddate(start,end):
-    # date = session.query(Measurement.date).all()
+
+@app.route("/api/v1.0/<start_date>/<end_date>")
+def start(start_date="",end_date=""):
+    if end_date != None:
+        start = dt.datetime.strptime(start_date, "%m-%d-%Y").date()
+        end = dt.datetime.strptime(end_date, "%m-%d-%Y").date()
+        
+        start_end_data = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs),func.avg(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+        
+        start_date_list = list(start_end_data)
+    else:
+        start = dt.datetime.strptime(start_date, "%m-%d-%Y").date()
+        
+        start_data = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs),func.avg(Measurement.tobs)).filter(Measurement.date >= start).all()
+        
+        start_date_list = list(start_data)
+    
+    return jsonify(start_date_list[0])
+
+
+ #   select_date=[08-08]
+    # canonicalized = start.replace({select_date}).datetime()
+    # for date in temp_data:
+    #     search_term = date[""].replace({select_date})
+    #     temp_date_dict={}
+    #     temp_date_dict['Low'] = session.query(func.min(measurement.tobs).filter(measurement.date >= search_term)
+
+
+    # if search_term == canonicalized:
+    #     return jsonify(select_date)
+
+    # return jsonify(f"Time frame with dates {select_date} not found.")
+
+
+
+
+
+    # date_of_temp = []
+    # for min,max,avg in temp_data:
+    #     date_of_temp.append(min)
+    #     date_of_temp.append(max)
+    #     date_of_temp.append(avg)
+    
+    # return jsonify(date_of_temp)
+   # date = session.query(Measurement.date).all()
     # low_temp = session.query(func.min(Measurement.tobs)).all()
     # high_temp = session.query(func.max(Measurement.tobs)).all()
     # avg_temp = session.query(func.avg(Measurement.tobs)).all()
     # avg_temp = round(avg_temp[0][0], 2)
     # temp_data = list(low_temp, high_temp, avg_temp)
     
-    if end != None:
-        temp_data = session.query(func.min(measurement.tobs), (func.max(measurement.tobs),(func.avg(measurement.tobs)).\
-            filter(measurement.date <= end).\
-            filter(measurement.date >= start).all()
-    else:
-        temp_data = session.query(func.min(measurement.tobs), (func.max(measurement.tobs),(func.avg(measurement.tobs)).\
-            filter(measurement.date >= start).all()
-
-#     date=[]
-#     canonicalized = start.replace("").datetime()
-#     for date in temp_data:
-#         search_term = date[""].replace("")
-#         temp_date_dict={}
-#         temp_date_dict['Low'] = session.query(func.min(measurement.tobs).filter(measurement.date >= canonicalized)
-
-# #
-# #        if search_term == canonicalized:
-# #            return jsonify(date)
-
-#     return jsonify({"error": f"Character with real_name {real_name} not found."}), 404
-
-
-
-
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
